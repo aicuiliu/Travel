1f4f1b0b19359f15c3690c5e9afd00c035a1db8f
lixupeng <lxp1901100540@163.com>
2018-12-20 00:40:05 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import {swiper, swiperSlide} from 'vue-awesome-swiper'
+
+  export default {
+    name: "Icons",
+    data () {
+      return {
+        iconList: [
+          {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+            desc: '一日游'
+          },
+          {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+            desc: '上海必游'
+          },
+          {
+            id: '0004',
+            imgUrl: 'https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png',
+            desc: '上海海昌'
+          },
+          {
+            id: '0005',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f6/e54fad3ea337b02.gif',
+            desc: '年终大促'
+          },
+          {
+            id: '0006',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+            desc: '泡温泉'
+          },
+          {
+            id: '0007',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+            desc: '上海欢乐谷'
+          },
+          {
+            id: '0008',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+            desc: '野生动物园'
+          },
+          {
+            id: '0009',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
+            desc: '上海迪士尼'
+          },
+          {
+            id: '00010',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+            desc: '全部玩乐'
+          }
+        ]
+      }
+    },
+    computed: {
+      pages () {
+        const pages = []
+        this.iconList.forEach((item,index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+
+        return pages;
+      }
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import "~styles/mixins.styl"
+
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    position: relative
+    width: 25%
+    height 0
+    float: left
+    padding-bottom: 25%
+    overflow: hidden
+
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
+</style>
